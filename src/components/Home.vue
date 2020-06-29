a9ffa04250e84e97b69b5e486dbc43e1ff037c9f
Henrik Foogel <henrik.foogel@gmail.com>
2020-06-29 19:46:36 +0200

    updated npm

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,18 +9,20 @@
         <img src="../assets/HenrikFoogel.jpg" alt="">
         <div class="home-text">
           Welcome,
-          My name is Henrik and I recently graduated as a Frontend-developer from Medieinstitutet in Gothenburg. My primary focus has been JavaScript with Vuejs as Framework. <br> 
-          During my studies I've also learned NodeJs, MongoDB, PHP, SQL and NoSQL. Included in the sudies was also two terms of intenships.<br>
-          <br>During the first internship I was at VGR / Digital Media where I got to work mostly with Wordpress / HTML / CSS, but also a little bit of Python and Javascript.
-          <br><br>During the second internship I had I was at a company called Quality Think, where I got to work with Javascript / Vue.js / React Native / Wordpress / HTML / Sass.
-          <br><br>After my studies ended I started an internship at another company called Flourish Feedback where I work with Javascript / Vue.js / Sass. I have references from all three internships.
-          <br>As a person I'm calm, reliable and hard working. I always help out where I'm needed and do what I can for others.
+          <br>My name is Henrik and I recently graduated as a Frontend-developer from Medieinstitutet in Gothenburg. My primary focus is JavaScript with Vue.js as Framework. <br> 
+          I have also studied NodeJs, MongoDB, PHP, SQL, NoSQL and had two terms of internships.<br>
+          <br>I had my first internship at Västra GötalandsRegionen/Digital Media where I worked mostly with Wordpress, HTML and CSS, but also did a little bit of Python and Javascript.
+          <br><br>The second internship I had was at a company called Quality Think, where I worked with Javascript, Vue.js, React Native, Wordpress, HTML and Sass.
+          <br><br>After I graduated I started an internship at a company called Flourish Feedback where I work with Javascript, Vue.js and Sass. 
+          <br>I've also been working on a project made with React during my freetime.<br><br> I have references from all three internships.
+          <br>
+          <br>I'm a calm, reliable and hard working person. I always help out where I'm needed and do what I can for others.
         </div>
         <div class="home-open-projects-menu-btn">Links to some of my stuff</div>
         <section class="home-project-buttons">
           <div class="home-project-buttons-container">
           <a class="home-project-btn fa fa-film" href="http://henrik-foogel.com/projects/mymovieshelf/#/"></a>
-          <p>Your Movie Shelf</p>
+          <p>My Movie Shelf</p>
           </div>
           <div class="home-project-buttons-container">
           <div class="home-project-btn fa fa-file-image-o" @click="$router.push('/gallery')"></div>
